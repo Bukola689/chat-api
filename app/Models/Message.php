c23864290ed4f385c29599df39a157c1ed5f2395
Bukola689 <jbukola95@gmail.com>
2025-09-18 16:30:37 +0100

    add relationship too message

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    protected $fillable = ['conversation_id','user_id','body','attachment','status'];
+
+    public function conversation()
+    {
+        return $this->belongsTo(Conversation::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }

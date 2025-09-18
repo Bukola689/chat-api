3af5d5b14903bec124e0f9fc5f891fff1ca3d0df
Bukola689 <jbukola95@gmail.com>
2025-09-18 16:34:54 +0100

    add relationship and install santum for users

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -38,13 +39,13 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-//    public function conversations()
-//    {
-//        return $this->belongsToMany(Conversation::class)->withTimestamps();
-//    }
+   public function conversations()
+   {
+       return $this->belongsToMany(Conversation::class)->withTimestamps();
+   }
 
-//    public function messages()
-//    {
-//       return $this->hasMany(Message::class);
-//    }
+   public function messages()
+   {
+      return $this->hasMany(Message::class);
+   }
 }
